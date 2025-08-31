f00a0af834ea3eedb1cf1bb070dec75829c14a52
Rahul Tiwari <128848653+rrtiwari@users.noreply.github.com>
2025-08-31 10:55:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 const container = document.querySelector('.items');
 let isDown = false;
-let startX;
-let scrollLeft;
+let startPageX = 0;
+let startScrollLeft = 0;
 
 container.addEventListener('mousedown', (e) => {
   isDown = true;
   container.classList.add('active');
-  startX = e.pageX - container.offsetLeft;
-  scrollLeft = container.scrollLeft;
+  startPageX = e.pageX;
+  startScrollLeft = container.scrollLeft;
 });
 
-container.addEventListener('mouseleave', () => {
-  isDown = false;
-  container.classList.remove('active');
+container.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+  const dx = e.pageX - startPageX;
+  container.scrollLeft = startScrollLeft - dx;
 });
 
 container.addEventListener('mouseup', () => {
@@ -20,11 +21,7 @@ container.addEventListener('mouseup', () => {
   container.classList.remove('active');
 });
 
-container.addEventListener('mousemove', (e) => {
-  if (!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - container.offsetLeft;
-  const walk = (x - startX) * 2;
-  container.scrollLeft = scrollLeft - walk;
+container.addEventListener('mouseleave', () => {
+  isDown = false;
+  container.classList.remove('active');
 });
-
